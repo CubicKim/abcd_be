c73efff12da77d8af80a80d0dd874e4079a01161
CubicKim <rlatpgns1019@cubicinc.co.kr>
2026-06-02 10:29:43 +0900

    이전 상황으로

--- src/main/java/com/example/demo/controller/DataController.java
+++ src/main/java/com/example/demo/controller/DataController.java
@@ -18,18 +18,18 @@ public class DataController {
     @Autowired
     private SampleMapper sampleMapper;
 
-//    @GetMapping("/data")
-//    public Map<String, Object> getBackendData() {
-//        Map<String, Object> response = new HashMap<>();
-//        response.put("status", "success");
-//        response.put("message", "Hello from Spring Boot Backend (abcd_be)!");
-//        response.put("version", "JDK 17 on Kubernetes (com.example.demo)");
-//        return response;
-//    }
-
     @GetMapping("/data")
-    public SampleVO getBackendData() {
-        // 🎯 하드코딩 대신 진짜 DB에서 꺼내온 데이터를 리턴합니다!
-        return sampleMapper.getFirstSample();
+    public Map<String, Object> getBackendData() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "success");
+        response.put("message", "Hello from Spring Boot Backend (abcd_be)!");
+        response.put("version", "JDK 17 on Kubernetes (com.example.demo)");
+        return response;
     }
+
+//    @GetMapping("/data")
+//    public SampleVO getBackendData() {
+//        // 🎯 하드코딩 대신 진짜 DB에서 꺼내온 데이터를 리턴합니다!
+//        return sampleMapper.getFirstSample();
+//    }
 }
